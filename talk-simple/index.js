79816aab1eaad75b6f220bba80ef989f9fa11460
zhengwj <516114697@qq.com>
2021-06-24 23:41:45 +0800

    feat:git merge

--- talk-simple/index.js
+++ talk-simple/index.js
@@ -1,43 +1,103 @@
-/* var inquirer = require('inquirer')
-function replaceStr (str) {
-	str=str.replace("吗", "")
-	str=str.replace("谁", "小可爱")
-	str=str.replace("你", "我")
-	str=str.replace("？", "!")
-	str=str.replace("?", "!")
-	return str
-}
-function talkBegin () {
-	inquirer
-		.prompt([
-			{
-				type: 'input',
-				name: 'theme',
-				message: '输入你的问题?'
-			}
-		])
-		.then(answers => {
-			console.log(replaceStr(answers.theme))
-			talkBegin()
-		})
-}
-talkBegin() */
-
-/* (async function () {
-  var Git = require("nodegit")
-  // var path = require("path")
-  let tmp = await Git.Repository.open('.')
-  console.log(tmp)
-  // let newBranch = await Git.Branch.create(tmp, 'develop')
-  // console.log(newBranch)
-  let tmp1 = await tmp.createBranch('develop', '测试1', 1)
-  console.log(tmp1)
-})() */
-
 const childProcess = require('child_process')
-const branch = childProcess.execSync('git rev-parse --abbrev-ref HEAD').toString().replace(/\s+/, '')
+// const argv = require('yargs').alias('m', 'mr').alias('c', 'cr').boolean(['mr', 'cr']).argv
+const argv = require('yargs')
+  .option('mr', {
+    alias : 'm',
+    describe: '创建合并请求',
+    boolean: true
+  })
+  .option('cr', {
+    alias : 'c',
+    describe: '解决冲突',
+    boolean: true
+  })
+  .option('ta', {
+    alias : 't',
+    describe: '制定mr时默认的分支',
+    type: 'string'
+  })
+  .example('bd --mr', '创建合并请求')
+  .example('bd --cr', '解决冲突')
+  .example('bd --ta release', '指定release为要merge request到的分支')
+  .help('h')
+  .alias('h', 'help')
+  .epilog('copyright @ 2021 bd')
+  .argv
+const inquirer = require('inquirer')
+const openDefaultBrowser = function (url) {
+  var exec = childProcess.exec
+  switch (process.platform) {
+    case "darwin":
+      exec('open ' + url)
+      break;
+    case "win32":
+      exec('start ' + url)
+      break;
+    default:
+      exec('xdg-open', [url])
+  }
+}
+const workBranch = childProcess.execSync('git rev-parse --abbrev-ref HEAD').toString().replace(/\s+/, '')
+const conflictTargetBranch = 'conflict-' + workBranch
+let currentRe = childProcess.execSync('git remote -v').toString()
+currentRe = currentRe.split('\n')[0].split(currentRe.includes('@') ? '@' : '//')[1].replace('.git (fetch)', '').replace(':', '/')
+// return
+// origin  git@github.com:Mical-kiss/talk-simple.git (fetch)
+// origin  http://git.dev.biaodianyun.com/biaodianyun/ds_shopadmin_pc.git (fetch)
+// origin  git@git.dev.biaodianyun.com:biaodianyun/ds_shopadmin_pc.git (fetch)
+// http://git.dev.biaodianyun.com/biaodianyun/ds_shopadmin_pc/-/merge_requests/new?merge_request[source_branch]=backup/9cell&merge_request[target_branch]=master
 
-console.log(childProcess.execSync('git checkout develop'))
-console.log(childProcess.execSync('git pull'))
-console.log(childProcess.execSync('git checkout -b ' + branch + 'dev'))
-console.log(childProcess.execSync('git merge ' + branch))
+if (argv.mr) {
+  creatMR()
+} else if (argv.cr) {
+  fixConflict()
+} else {
+  inquirer.prompt([{
+    type: 'list',
+    name: 'actionName',
+    message: '选择要进行的git操作',
+    choices: [
+      "解决冲突",
+      "创建MR"
+    ]
+  }]).then(answers => {
+    switch (answers.actionName) {
+      case '解决冲突':
+        fixConflict()
+        break;
+      case '创建MR':
+        creatMR()
+        break;
+      default:
+        break;
+    }
+  })
+}
+function fixConflict () {
+  childProcess.execSync('git branch -D develop')
+  childProcess.execSync('git checkout develop')
+  childProcess.execSync('git checkout -b ' + conflictTargetBranch)
+  childProcess.execSync('git push --set-upstream origin ' + conflictTargetBranch)
+  childProcess.execSync('git merge ' + workBranch)
+  creatMR()
+}
+
+function creatMR () {
+  if (argv.ta) {
+    // console.log(`http://${currentRe}/-/merge_requests/new?merge_request[source_branch]=${workBranch}&merge_request[target_branch]=${argv.ta}`)
+    openDefaultBrowser(encodeURI(`http://${currentRe}/-/merge_requests/new?merge_request[source_branch]=${workBranch}&merge_request[target_branch]=${argv.ta}`))
+  } else {
+    inquirer.prompt([{
+      type: 'list',
+      name: 'actionName',
+      message: '选择要到合并的分支',
+      choices: [
+        "release",
+        "release_payment"
+      ]
+    }]).then(answers => {
+      console.log(answers)
+      openDefaultBrowser(`http://${currentRe}/-/merge_requests/new?merge_request[source_branch]=${workBranch}&merge_request[target_branch]=${answers.actionName}`)
+    })
+  }
+}
